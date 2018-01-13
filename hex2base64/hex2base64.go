c9d5ff281d4caf898cac359e2b50db63a25a6dcf
stravis <sltravis@centurylink.net>
2018-01-13 11:34:55 -0700

    more refactoring of hex2base64.go

--- hex2base64/hex2base64.go
+++ hex2base64/hex2base64.go
@@ -3,10 +3,38 @@ package hex2base64
 import "fmt"
 
 func Hex2base64(hex string) string {
-	// using this inline assignment for now to test the conversion
-	// will replace with a parameter to read a file
-	//data := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
+	fmt.Println("Hex string = ", hex)
+
+	hexbinstr := Hex2bin(hex)
+	// print output to see binary data
+	/*
+		fmt.Println("Binary string = ", hexbinstr)
+		fmt.Println("Binary string length = ", len(hexbinstr))
+	*/
+
+	var base64output string
+	base64output = Bin2base64(hexbinstr)
+	return (base64output)
+
+}
+
+func Hex2bin(hex string) string {
+	var hexDec = map[byte]string{'0': "0000", '1': "0001", '2': "0010",
+		'3': "0011", '4': "0100", '5': "0101", '6': "0110", '7': "0111",
+		'8': "1000", '9': "1001", 'a': "1010", 'b': "1011", 'c': "1100",
+		'd': "1101", 'e': "1110", 'f': "1111"}
+
+	// Create a binary string out of the hex data
+	var hexbinstr string
+
+	for i := 0; i < len(hex); i++ {
+		hexbinstr = hexbinstr + hexDec[hex[i]]
+	}
+	return (hexbinstr)
+}
+
+func Bin2base64(hexbinstr string) string {
 	var base64enc = map[string]string{"000000": "A", "000001": "B", "000010": "C",
 		"000011": "D", "000100": "E", "000101": "F", "000110": "G", "000111": "H",
 		"001000": "I", "001001": "J", "001010": "K", "001011": "L", "001100": "M",
@@ -21,27 +49,6 @@ func Hex2base64(hex string) string {
 		"110101": "1", "110110": "2", "110111": "3", "111000": "4", "111001": "5",
 		"111010": "6", "111011": "7", "111100": "8", "111101": "9", "111110": "+",
 		"111111": "/"}
-
-	var hexDec = map[byte]string{'0': "0000", '1': "0001", '2': "0010",
-		'3': "0011", '4': "0100", '5': "0101", '6': "0110", '7': "0111",
-		'8': "1000", '9': "1001", 'a': "1010", 'b': "1011", 'c': "1100",
-		'd': "1101", 'e': "1110", 'f': "1111"}
-
-	// Create a binary string out of the hex data
-	var hexbinstr string
-
-	for i := 0; i < len(hex); i++ {
-		hexbinstr = hexbinstr + hexDec[hex[i]]
-	}
-
-	fmt.Println("Hex string = ", hex)
-	fmt.Println("")
-
-	// print output to see binary data
-	/*
-		fmt.Println("Binary string = ", hexbinstr)
-		fmt.Println("Binary string length = ", len(hexbinstr))
-	*/
 
 	var binBase64 string
 	var base64output string
@@ -74,7 +81,6 @@ func Hex2base64(hex string) string {
 
 	}
 
-	fmt.Println("base64output = ", base64output)
+	//fmt.Println("base64output = ", base64output)
 	return (base64output)
-
 }
--- test.go
+++ test.go
@@ -7,5 +7,5 @@ func main(){
   data := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
   fmt.Println(data)
-  fmt.Println("this ", hex2base64.Hex2base64(data))
+  fmt.Println("base64 =  ", hex2base64.Hex2base64(data))
 }
